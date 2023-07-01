c10643a4a424b7da93bcb2394e8c03812f6da173
Vansh Bansal <vanshbansal@Vanshs-MacBook-Air.local>
2023-07-02 00:08:12 +0530

    first commit

---
+++ app/api/prompt/[id]/route.js
@@ -0,0 +1,44 @@
+import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
+
+export const GET = async (request,{params})=>{
+    try {
+        await connectToDB()
+        const prompt = await Prompt.findById(params.id).populate("creator")
+        if(!prompt) return new Response("Prompt not found",{status:404})
+
+        return new Response(JSON.stringify(prompt),{status:200})
+    } catch (error) {
+        return new Response("Internal server error",{status:500})
+    }
+}
+
+export const PATCH = async (request,{params})=>{
+    const {prompt,tag} = await request.json();
+
+    try {
+        await connectToDB()
+        const existingPrompt = await Prompt.findById(params.id);
+        if(!existingPrompt){
+            return new Response("Prompt not found", { status: 404 });
+        }
+        existingPrompt.prompt = prompt
+        existingPrompt.tag = tag;
+        await existingPrompt.save()
+        return new Response("Successfully updated the prompt",{status:200})
+    } catch (error) {
+        return new Response("Error updating prompt",{status:500})
+    }
+}
+
+
+export const DELETE = async (request,{params})=>{
+    try {
+        await connectToDB()
+
+        await Prompt.findByIdAndDelete(params.id)
+        return new Response("Prompt deleted successfully",{status:200})
+    } catch (error) {
+        return new Response("Error deleting prompt",{status:500})
+    }
+}
